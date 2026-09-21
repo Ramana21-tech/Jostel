e706651944fafb2364b3ef90fbceba0b5e0387a9
Ramana-vs <vsramana02@gmail.com>
2026-09-21 20:34:00 +0530

    Delete js/services directory

--- js/services/footer.js
+++
@@ -1,8 +0,0 @@
-
-export function renderFooter() {
-  return `
-    <footer class="footer">
-      JosTEL — Joseph's Technology Enhanced Learning.
-      Discover → Try → Think → Assess → Capture → Reflect.
-    </footer>`;
-}
